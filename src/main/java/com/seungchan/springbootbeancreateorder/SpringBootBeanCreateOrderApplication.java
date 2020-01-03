326c3efa1829140ec088d5ee32d7b1d2f3e3770a
baegseungchan <baegseungchan@nm-itsgames.com>
2020-01-03 10:15:12 +0900

    init

---
+++ src/main/java/com/seungchan/springbootbeancreateorder/SpringBootBeanCreateOrderApplication.java
@@ -0,0 +1,13 @@
+package com.seungchan.springbootbeancreateorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBeanCreateOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBeanCreateOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seungchan/springbootbeancreateorder/SpringBootBeanCreateOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seungchan.springbootbeancreateorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBeanCreateOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
